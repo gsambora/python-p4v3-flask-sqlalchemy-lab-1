c5a9bb7ff33cbeaa27ff25761b9474086a19fa08
Hana Sambora <grace.sambora@gmail.com>
2024-02-09 13:20:21 -0600

    Complete lab

--- server/app.py
+++ server/app.py
@@ -22,6 +22,26 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def quake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def quake_by_magnitude(magnitude):
+    quakes = []
+
+    for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append(quake.to_dict())
+    
+    return {'count': len(quakes), 'quakes': quakes}
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
